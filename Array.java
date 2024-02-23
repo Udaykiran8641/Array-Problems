dacf005a5e245929db122ba6e2b7e955352269dc
Udaykiran8641 <142726275+Udaykiran8641@users.noreply.github.com>
2024-02-23 09:49:09 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,7 @@
+// Java program To Find the length of an Array
+public class Array {
+    public static void main(String[] args) {
+        int[] array = new int[] {1,2,3,4,5,6,5,4};
+        System.out.println("Number of elements present in an array "+array.length);
+    }
+}
